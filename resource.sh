fc51f2ab5308370ed20dbbd3ffe35070e85bfb5e
Hariteja82 <141202181+Hariteja82@users.noreply.github.com>
2025-03-06 22:47:29 +0530

    Update resource.sh

--- resource.sh
+++ resource.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo yum update -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.keysudo 
 sudo yum update -y
 sudo yum install java-17-amazon-corretto -y
