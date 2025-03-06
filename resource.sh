e30b58f776b87b2e6a235d1865374f0e98fbce27
Hariteja82 <141202181+Hariteja82@users.noreply.github.com>
2025-03-06 22:58:56 +0530

    Update resource.sh

--- resource.sh
+++ resource.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum update -y
-sudo sudo wget -O /etc/yum.repos.d/jenkins.repo \
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.keysudo 
 sudo yum update -y
